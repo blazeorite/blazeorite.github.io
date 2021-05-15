6ef865ea446313e47e5920a8b80770b5886a0e9a
sujith <sujith0709@gmail.com>
2021-05-15 00:45:41 -0400

    Updates

---
+++ precache-manifest.05eaed27129edd1a9621be3d44756edd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab6d57ae4a8ca1fc1f4c40cc8f37e14",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "b4c6bf9ea4289f65f249",
+    "url": "/Portfolio-Website/static/css/2.9a766daa.chunk.css"
+  },
+  {
+    "revision": "05677c3414c5b206cd54",
+    "url": "/Portfolio-Website/static/css/main.9d1d1a0e.chunk.css"
+  },
+  {
+    "revision": "b4c6bf9ea4289f65f249",
+    "url": "/Portfolio-Website/static/js/2.89411143.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Portfolio-Website/static/js/2.89411143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05677c3414c5b206cd54",
+    "url": "/Portfolio-Website/static/js/main.facf37dc.chunk.js"
+  },
+  {
+    "revision": "ec2a8c4a5b521bfadeb7",
+    "url": "/Portfolio-Website/static/js/runtime-main.55d7bdd1.js"
+  },
+  {
+    "revision": "4111f7a7e505f9b158110e7b35bfe22e",
+    "url": "/Portfolio-Website/static/media/cc1.4111f7a7.jpeg"
+  },
+  {
+    "revision": "a388ffe6735d5dc7c906c94a9538bb8d",
+    "url": "/Portfolio-Website/static/media/cc2.a388ffe6.jpeg"
+  },
+  {
+    "revision": "6ccd978665a21a96a7f11484c844a1ae",
+    "url": "/Portfolio-Website/static/media/profile-pic.6ccd9786.jpg"
+  },
+  {
+    "revision": "ec019f1b795b33b05a0cb6de6e8c363b",
+    "url": "/Portfolio-Website/static/media/resume.ec019f1b.pdf"
+  }
+]);
